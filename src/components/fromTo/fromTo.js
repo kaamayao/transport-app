7910ef02a7a04886b0d3ac8b7a72a7c28bc95712
kaamayao <kevinamaya04@gmail.com>
2020-07-16 19:56:52 -0500

    second commit

--- src/components/fromTo/fromTo.js
+++ src/components/fromTo/fromTo.js
@@ -2,8 +2,23 @@ import React from 'react';
 import './fromTo.scss';
 
 export default class FromTo extends React.Component {
+  state = {
+    from: '',
+    to: '',
+    Time: '',
+  }
+
   constructor(props) {
     super(props);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  handleFormChange(event) {
+    const form = this.state.form;
+    form[event.target.name] = event.target.value;
+    this.setState(
+        {form: form},
+    );
   }
 
   render() {
@@ -12,15 +27,18 @@ export default class FromTo extends React.Component {
         <div className="fromTo__location-container">
           <div className="fromTo__wrap">
             <label className="fromTo__label">From</label>
-            <input className="fromTo__fromTxt" type="text" />
+            <input className="fromTo__fromTxt" type="text"
+                    onChange={this.handleFormChange}/>
             <label className="fromTo__label">to</label>
-            <input className="fromTo__toTxt" type="text" />
+            <input className="fromTo__toTxt" type="text"
+                    onChange={this.handleFormChange}/>
           </div>
         </div>
         <div className="fromTo__time-wrap">
           <div className="fromTo__wrap">
             <label className="fromTo__label">Time</label>
-            <input className="fromTo__timeTxt" type="text" />
+            <input className="fromTo__timeTxt" type="text"
+                    onChange={this.handleFormChange}/>
           </div>
         </div>
       </div>
